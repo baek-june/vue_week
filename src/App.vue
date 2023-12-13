c353dc4fc70796921472109bc9de7855bf977652
100june <toypiano@naver.com>
2023-12-13 19:02:22 +0900

    dataBinding

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/databindingTextarea">Data Binding Textarea</router-link>
   </nav>
   <router-view/>
 </template>
